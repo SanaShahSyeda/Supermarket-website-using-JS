54ce18a4dbe46c4de0178ff579abd85528fdc6e6
Syeda Sana <sana.shah1740@gmail.com>
2023-07-13 22:46:18 -0700

    used javascript to get value provided in searchbar and scroll to that portion of "meat categorie."

--- js/meats.js
+++ js/meats.js
@@ -1,20 +1,31 @@
-var beef_q=0;
-var chicken_q=0;
-var mutton_q=0;
+var beef_q = 0;
+var chicken_q = 0;
+var mutton_q = 0;
 
-console.log('hello World !!');
+document
+  .getElementById("form-search")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+    var input = document.getElementById("search-bar");
+    var value = input.value;
+    var categories = document.querySelectorAll("#meats > div");
+    var found=false;
+    for (var i = 0; i < categories.length; i++) {
+      var category = categories[i];
+      var categoryName = category.getAttribute("id");
+      if (categoryName.includes(value)) {
+        category.scrollIntoView({ behavior: "smooth" });
+        found=true;
+        break;
+      }
+      if (!found) {
+        alert("Category not found.");
+      }
+    }
+  });
 
-var abc = document.querySelector("input").value;
-console.log(abc);
+function add_quantity() {}
 
-function add_quantity(){
+function sub_quantity() {}
 
-}
-
-function sub_quantity(){
-   
-}
-
-function addCartStatus(){
-
-}
+function addCartStatus() {}
