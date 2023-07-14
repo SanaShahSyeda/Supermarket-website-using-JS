1a148324575c6f2c82ff50b5a81af6f527e8fc86
Syeda Sana <sana.shah1740@gmail.com>
2023-07-13 23:44:00 -0700

    extracted span value of quantity under each sub-categorie of "meat".

--- js/meats.js
+++ js/meats.js
@@ -1,31 +1,54 @@
-var beef_q = 0;
-var chicken_q = 0;
-var mutton_q = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  //   var beef_q = 0;
+  //   var chicken_q = 0;
+  //   var mutton_q = 0;
 
-document
-  .getElementById("form-search")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-    var input = document.getElementById("search-bar");
-    var value = input.value;
-    var categories = document.querySelectorAll("#meats > div");
-    var found=false;
-    for (var i = 0; i < categories.length; i++) {
-      var category = categories[i];
-      var categoryName = category.getAttribute("id");
-      if (categoryName.includes(value)) {
-        category.scrollIntoView({ behavior: "smooth" });
-        found=true;
-        break;
+  // get value of quantity
+  var spans = document.getElementsByClassName("quantity");
+  for (var i = 0; i < spans.length; i++) {
+    var value = spans[i].textContent;
+    console.log(value);
+    if (value == 1) {
+      console.log("value in if -----");
+      var link = document.getElementsByClassName("minus");
+      var image = document.getElementsByClassName("minus-image");
+
+      if (link) {
+        link.addEventListener("click", function (event) {
+          event.preventDefault();
+        });
       }
-      if (!found) {
-        alert("Category not found.");
+      if (image) {
+        image.addEventListener("click", function (event) {
+          event.preventDefault();
+        });
       }
     }
-  });
+  }
 
+  // get value of search-bar and scroll to intended portion.
+  document
+    .getElementById("form-search")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent form submission
+      var input = document.getElementById("search-bar");
+      var value = input.value;
+      var categories = document.querySelectorAll("#meats > div");
+      var found = false;
+      for (var i = 0; i < categories.length; i++) {
+        var category = categories[i];
+        var categoryName = category.getAttribute("id");
+        if (categoryName.includes(value)) {
+          category.scrollIntoView({ behavior: "smooth" });
+          found = true;
+          break;
+        }
+        if (!found) {
+          alert("Category not found.");
+        }
+      }
+    });
+});
 function add_quantity() {}
-
-function sub_quantity() {}
-
-function addCartStatus() {}
+  function sub_quantity() {}
+  function addCartStatus() {}
