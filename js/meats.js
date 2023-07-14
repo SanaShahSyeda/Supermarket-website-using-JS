2477a210086b30c925921c5b5780ffefb48630c2
Syeda Sana <sana.shah1740@gmail.com>
2023-07-13 21:44:33 -0700

    "onclick" applied to show  button of all categories in index.html file.

--- js/meats.js
+++ js/meats.js
@@ -2,12 +2,13 @@ var beef_q=0;
 var chicken_q=0;
 var mutton_q=0;
 
+console.log('hello World !!');
 
 var abc = document.querySelector("input").value;
 console.log(abc);
 
 function add_quantity(){
-  
+
 }
 
 function sub_quantity(){
