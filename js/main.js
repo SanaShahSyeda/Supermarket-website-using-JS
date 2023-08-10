7da9de4d2fb762307c339f63e06ebc7bef94fcfb
Syeda Sana <sana.shah1740@gmail.com>
2023-08-10 00:59:56 -0700

    styling changes to index.html

--- js/main.js
+++ js/main.js
@@ -14,18 +14,18 @@ function frozenfoods_categorie(){
    window.open("frozenfoods.html");
 }
 
-function cereals_categorie(){
-   window.open("cereals.html");
-}
+// function cereals_categorie(){
+//    window.open("cereals.html");
+// }
 
-function breadbakery_categorie(){
-   window.open("breadbakery.html");
-}
+// function breadbakery_categorie(){
+//    window.open("breadbakery.html");
+// }
 
-function dairy_categorie(){
-   window.open("dairy.html");
-}
+// function dairy_categorie(){
+//    window.open("dairy.html");
+// }
 
-function drinks_categorie(){
-   window.open("drinks.htm");
-}
+// function drinks_categorie(){
+//    window.open("drinks.htm");
+// }
